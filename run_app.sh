7bb2c2a60a7113d90079b911cea2fa5afcf88320
Elena Camero <ecamero92@gmail.com>
2021-09-21 00:48:52 +0200

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker run --name postgresdb -p 5435:5432 -e POSTGRES_PASSWORD=postgres
+python database/init_db.py
 source app_env/bin/activate
 pip3 install -r requirements.txt
 gunicorn app:app -b 0.0.0.0:8000
