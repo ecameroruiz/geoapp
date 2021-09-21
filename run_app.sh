49970be1a021dacfbb6bf6ee07578b122ae56151
Elena Camero <ecamero92@gmail.com>
2021-09-21 12:49:06 +0200

    init script

--- run_app.sh
+++ run_app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker run --name postgresdb -p 5435:5432 -e POSTGRES_PASSWORD=postgres pgrouting/pgrouting:11-2.5-2.6.3
+docker run -d --name postgresdb -p 5435:5432 -e POSTGRES_PASSWORD=postgres pgrouting/pgrouting:11-2.5-2.6.3
 docker exec -it postgresdb psql -h localhost -U postgres -c "CREATE EXTENSION postgis;"
 ./database/init_db.py
-python -m venv app_env
+python3.8 -m venv app_env
 source app_env/bin/activate
-pip3 install -r requirements.txt
+pip3.8 install -r requirements.txt
 gunicorn app:app -b 0.0.0.0:8000
